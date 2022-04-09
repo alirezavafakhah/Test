5f28cd42ac4ba91cf4e0306a5738826b3bad5d9f
zavf <avf69programmer@gmail.com>
2022-04-09 12:54:25 +0430

    opkop

--- Dialogs.js
+++ Dialogs.js
@@ -6,19 +6,73 @@ console.log(confirm('Question, Do You want Continue?'));
 console.log(prompt('Whats Your Name?'));
 
 
-document.querySelector("#h2class").onclick= function (){
-    alert('fgfg')
-};
-
-let gethe2= document.getElementById('h2class');
-gethe2.onmouseenter = function (event){
-    event.target.style.backgroundColor='green';
-};
-
-gethe2.onmouseleave = function (event){
-    event.target.style.backgroundColor='white';
-};
+// با استفاده از eventhandle
+// document.querySelector("#h2class").onclick= function (){
+//     alert('fgfg')
+// };
+//
+// let gethe2= document.getElementById('h2class');
+// gethe2.onmouseenter = function (event){
+//     event.target.style.backgroundColor='green';
+// };
+//
+// gethe2.onmouseleave = function (event){
+//     event.target.style.backgroundColor='white';
+// };
 
 // document.getElementById("h2class").onclick= function (){
 //     alert('fgfg')
 // };
+
+
+// // با استفاده از event listner
+// let geth2 = document.getElementById('h2class');
+//
+// geth2.addEventListener('click', function (){
+//     alert('by listner')
+// });
+//
+// geth2.addEventListener('click', function (){
+//     alert('by listner 2')
+// });
+
+
+let el1 = document.querySelector('.el1');
+let el2 = document.querySelector('.el2');
+
+
+el1.addEventListener('click', function (event) {
+
+    console.log('Shape Father Click');
+});
+
+
+// el2.addEventListener('click', function (e) {
+//     // کد زیر باعث میشود زمانیکه ایونت المنت پدر فراخوانی شد،ایونت فرزند اجرا نشود
+//     e.stopPropagation();
+//     console.log('Shape Child Click');
+// });
+
+
+document.querySelector('#link').addEventListener('click', function (e){
+    // باعث میشود کد ایونت اجرا نشود،پس لینک بارگذاری نمیشود
+    e.preventDefault();
+    console.log("clicked and don't run" );
+})
+
+el2.addEventListener('click', ele2fun);
+
+//شکل دیگر
+function ele2fun(e) {
+    // کد زیر باعث میشود زمانیکه ایونت المنت پدر فراخوانی شد،ایونت فرزند اجرا نشود
+    e.stopPropagation();
+    console.log('Shape Child Click');
+};
+
+
+// کد زیر پس از 10 ثانبه ایونت را پاک میکند
+setTimeout(() =>{
+    el2.removeEventListener('click',ele2fun);
+}, 10000)
+
+
